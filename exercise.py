bfb237bb783f674225d522185e5f89fc3f8eaa64
Widnie <widniedorilas890@yahoo.com>
2022-07-06 13:33:26 -0500

    part 1 complete

--- exercise.py
+++ exercise.py
@@ -4,28 +4,31 @@ from functools import reduce
 
 #PART 1
 #read in data from DP and translating into dictionaries
-reader = csv.DictReader(open("911_Calls_For_Service.csv"))
-
+reader = csv.DictReader(open("911_Calls_For_Service_(Last_30_Days).csv"))
+reader = list(reader)
+type(reader)
 #using filter with lambda functions to exclude
 #empty zip codes and neighborhoods
 exclusions = list(filter(lambda row: row["zip_code"] != "" and row["zip_code"] != "0" and row["zip_code"] != "Null" and
-row["neighborhood"] != "" and row["neighborhood"] != "Null" and row["neighborhood"] != "0" and 
-row["totalresponsetime"] != "" and row["totalresponsetime"] != "0" and row["totalresponsetime"] != "None" and
-row["dispatchtime"] != "" and row["dispatchtime"] != "0" and row["dispatchtime"] != "Null" and 
-row["totaltime"] != "" and row["totaltime"] != "0" and row["totaltime"] != "Null",reader))
+row["neighborhood"] != "" and row["neighborhood"] != "Null" and row["neighborhood"] != "0",reader))
 
 #using lambda functions and reduce to calculate averages
-responsetime = reduce(lambda rt1, reader: rt1 + float(reader["totalresponsetime"]), reader, 0)
-avgresponsetime = responsetime/len(exclusions)
+rtlist = list(filter(lambda row: row["totalresponsetime"] != "",reader ))
+responsetime = reduce(lambda rt1, reader: rt1 + float(reader["totalresponsetime"]),rtlist, 0)
+avgresponsetime = responsetime/len(rtlist)
 print(f"The average response time is {avgresponsetime}")
 
-dispatchtime = reduce(lambda dt1, reader: dt1 + float(reader["dispatchtime"]) * (reader["dispatchtime"] - 1), reader, 0)
-avgdispatchtime = dispatchtime/len(exclusions)
+dtlist = list(filter(lambda row: row["dispatchtime"] != "", reader))
+dispatchtime = reduce(lambda dt1, reader: dt1 + float(reader["dispatchtime"]), dtlist, 0)
+avgdispatchtime = dispatchtime/len(dtlist)
 print(f"The average dispatch time is {avgdispatchtime}")
 
-totaltime = reduce(lambda tt1, reader: tt1 + float(reader["totaltime"]) * (reader["totaltime"] - 1), reader,0)
-avgtotaltime = str(totaltime/len(exclusions))
+ttlist = list(filter(lambda row: row["totaltime"] != "",reader))
+type(ttlist)
+totaltime = reduce(lambda tt1, reader: tt1 + float(reader["totaltime"]), ttlist,0)
+avgtotaltime = totaltime/len(ttlist)
 print(f"The average total time is {avgtotaltime}")
- 
+
+
 
 #PART 2
